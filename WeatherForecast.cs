9f93cff5680f96a28c0d088f754d4d3ca4916759
cmnb32 <cmnmb981@gmail.com>
2025-09-29 12:22:27 -0600

    Initial Commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace skterminal_fuel_skids_api;
+
+public class WeatherForecast
+{
+  public DateOnly Date { get; set; }
+
+  public int TemperatureC { get; set; }
+
+  public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+  public string? Summary { get; set; }
+}
